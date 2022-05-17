5cc13d716cc9d0ed6acb6bbccb6c0dcf04f20409
little-boy52 <1144612318@qq.com>
2022-05-17 09:17:37 +0800

    Finish 2.0

--- WebApplication2/Second.aspx
+++ WebApplication2/Second.aspx
@@ -5,15 +5,134 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .style2
+        {
+            height: 22px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
     
-        <asp:Label ID="Data" runat="server"></asp:Label>
-        <asp:Image ID="Image1" runat="server" Height = "100" Width = "100" />
+        <table style="width: 100%; height: 329px;">
+            <tr>
+                <td>
+                    &nbsp;</td>
+                <td>
+                    用户修改</td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    用户名：</td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+                        ControlToValidate="TextBox1" ErrorMessage="此项必填"></asp:RequiredFieldValidator>
+                </td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="style2">
+                    密码：</td>
+                <td class="style2">
+                    <asp:TextBox ID="TextBox2" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+                        ControlToValidate="TextBox2" ErrorMessage="此项必填"></asp:RequiredFieldValidator>
+                    </td>
+                <td class="style2">
+                    </td>
+            </tr>
+            <tr>
+                <td>
+                    性别：</td>
+                <td>
+                    <asp:RadioButtonList ID="RadioButtonList1" runat="server" 
+                        RepeatDirection="Horizontal" Width="144px">
+                        <asp:ListItem Selected="True">男</asp:ListItem>
+                        <asp:ListItem>女</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    年龄：</td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server" TextMode="Number"></asp:TextBox>
+                    <asp:RangeValidator ID="RangeValidator1" runat="server" 
+                        ControlToValidate="TextBox3" ErrorMessage="范围一定" MaximumValue="150" 
+                        MinimumValue="0" Type="Integer"></asp:RangeValidator>
+                </td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    兴趣爱好：</td>
+                <td>
+                    <asp:DropDownList ID="DropDownList1" runat="server" 
+                        onselectedindexchanged="DropDownList1_SelectedIndexChanged">
+                        <asp:ListItem>唱</asp:ListItem>
+                        <asp:ListItem>跳</asp:ListItem>
+                        <asp:ListItem>Rap</asp:ListItem>
+                        <asp:ListItem>篮球</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    照片：</td>
+                <td>
+                <!--
+                    <asp:ImageButton ID="ImageButton1" runat="server" Height="60px" Width="104px" />
+                    <asp:FileUpload ID="FileUpload2" runat="server" />-->
+                    <asp:FileUpload ID="FileUpload1" runat="server"/>
+                    <asp:Button ID="btnUpload" Text="Upload" runat="server" OnClick="UploadFile"/>
+                    <hr />
+                    <asp:Image ID="Image1" runat="server" Height = "100" Width = "100" />
+                </td>
+                <td>
+                    &nbsp;</td>
+            </tr>
+        </table>
     
     </div>
+    <table style="width:100%;">
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                用户信息</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                <asp:Label ID="LblInfo" runat="server"></asp:Label>
+            </td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                <asp:Button ID="UpdateBtn" runat="server" Text="更新" onclick="UpdateBtn_Click"/>
+            </td>
+            <td>
+                &nbsp;</td>
+        </tr>
+    </table>
     </form>
 </body>
 </html>
--- WebApplication2/Second.aspx.cs
+++ WebApplication2/Second.aspx.cs
@@ -5,11 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace WebApplication2
 {
     public partial class Second : System.Web.UI.Page
     {
+        string id;
+        string name, psw, sex, age, like, img;
+        string sqlUpdate;
+        bool flag = false;
         protected void Page_Load(object sender, EventArgs e)
         {
             /*
@@ -26,20 +31,88 @@ namespace WebApplication2
             conn.Open();
             MySqlDataReader dr = comm.ExecuteReader();
             while (dr.Read()) {
-                string name = (String)dr[1].ToString().Trim();
-                string psw = (String)dr[2].ToString().Trim();
-                if (name == Session["userName"].ToString() && psw == Session["psw"].ToString()) { 
-                    Data.Text = "用户名：" + name;
-                    Data.Text += "<br>密码：" + psw;
-                    Data.Text += "<br>性别：" + (String)dr[3].ToString().Trim();
-                    Data.Text += "<br>年龄：" + (String)dr[4].ToString().Trim();
-                    Data.Text += "<br>爱好：" + (String)dr[5].ToString().Trim();
-                    Data.Text += "<br>照片：";
+                id = (String)dr[0].ToString().Trim();
+            }
+            conn.Close();
+            name = TextBox1.Text;
+            psw = TextBox2.Text;
+            sex = RadioButtonList1.SelectedValue;
+            age = TextBox3.Text;
+            like = DropDownList1.SelectedValue;
+            img = Image1.ImageUrl;
+            sqlUpdate = $"UPDATE `user`"+
+                        $" SET name = '{name}', psw = '{psw}', sex = '{sex}', age = '{age}', hobby = '{like}', imgUrl = '{img}'"+
+                        $" WHERE iduser = {id}; ";
+            if (flag) return;
+            flag = true;
+            sqlQuery = "SELECT * FROM user";
+            comm = new MySqlCommand(sqlQuery, conn);
+            conn.Open();
+            dr = comm.ExecuteReader();
+            while (dr.Read()) {
+                id = (String)dr[0].ToString().Trim();
+                string name2 = (String)dr[1].ToString().Trim();
+                string psw2 = (String)dr[2].ToString().Trim();
+                if (name2 == Session["userName"].ToString() && psw2 == Session["psw"].ToString()) { 
+                    TextBox1.Text = name2;
+                    TextBox2.Text = psw2;
+                    RadioButtonList1.SelectedValue = (String)dr[3].ToString().Trim();
+                    TextBox3.Text = (String)dr[4].ToString().Trim();
+                    DropDownList1.SelectedValue = (String)dr[5].ToString().Trim();
                     Image1.ImageUrl = (String)dr[6].ToString().Trim();
                     break;
                 }
             }
             conn.Close();
+        }
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void UploadFile(object sender, EventArgs e)
+        {
+            string folderPath = Server.MapPath("./Files/");
+
+            //Check whether Directory (Folder) exists.
+            if (!Directory.Exists(folderPath))
+            {
+                //If Directory (Folder) does not exists Create it.
+                Directory.CreateDirectory(folderPath);
+            }
+
+            //Save the File to the Directory (Folder).
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+            
+            //Display the Picture in Image control.
+            Image1.ImageUrl = "./Files/" + Path.GetFileName(FileUpload1.FileName);
+            //LblInfo.Text = Path.GetFullPath(FileUpload1.FileName);
+        }
+        protected void UpdateBtn_Click(object sender, EventArgs e) {
+            //读取的是旧的数据，是因为点击update后会刷新
+
+            string connection = "Server=localhost;Uid=root;Database=homework;Port=3306;";
+            MySqlConnection conn = new MySqlConnection(connection);
+            
+            //string sqlInsert = $"INSERT INTO user VALUES (NULL, '{name}', '{psw}', '{sex}', '{age}', '{like}', '{img}');";
+            //LblInfo.Text = sqlUpdate;
+            MySqlCommand comm = new MySqlCommand(sqlUpdate, conn);
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            string strName = "用户名：" + name;
+            string strPsw = "<br>密码：" + psw;
+            string strSex = "<br>性别：" + sex;
+            string strAge = "<br>年龄：" + age;
+            string strLike = "<br>爱好：" + like;
+            string strImg = "<br>照片：" + img;
+            string strEnd = "<br>修改成功";
+            LblInfo.Text = strName + strPsw + strSex + strAge + strLike + strImg + strEnd;
         }
     }
 }
